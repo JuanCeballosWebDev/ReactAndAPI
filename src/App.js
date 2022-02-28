24c7562819de7d3efd017e15ffdb17891af015a6
Juan Ceballos <ceballos.juan6@gmail.com>
2022-02-28 10:33:08 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const URL = "https://api.hatchways.io/assessment/students";
 function App() {
 
   const [studentData, setStudentData] = useState([]); 
+  const [data, setData] = useState("");
   
   const getStudents = (URL) => {
      fetch(URL)
@@ -18,17 +19,14 @@ function App() {
     getStudents(URL);
   }, []); 
 
-  
-  return (
-    <main>
-    <div>
-     {
-       studentData && 
-        studentData.map((student) => {
-          
-          let fullName = student.firstName + ' ' + student.lastName;
+  const filter = (stud, dat) => {
+    if (dat === '')
+    {
+      return (
+        stud.map((nam) => {
+          let fullName = nam.firstName + ' ' + nam.lastName;
           let total = 0;
-          let arrNumbers = student.grades;
+          let arrNumbers = nam.grades;
           for (let i = 0; i < arrNumbers.length; i++){
             total += Math.floor(arrNumbers[i]);
           }
@@ -36,17 +34,65 @@ function App() {
           
           return (
               <>
-                <img src={student.pic} />
-                <p style={{fontWeight: "bold"}}>{fullName}</p>
-                <p>Email: {student.email}</p>
-                <p>Company: {student.company}</p>
-                <p>Skill: {student.skill}</p>
-                <p>Average: {average}</p>
+                <img src={nam.pic} />
+                <div className="box">
+                  <p style={{fontWeight: "bold"}} className="name">{fullName}</p>
+                  <p>Email: {nam.email}</p>
+                  <p>Company: {nam.company}</p>
+                  <p>Skill: {nam.skill}</p>
+                  <p>Average: {average}</p>
+                </div>
               </>
           )
-
-        }) // From studentData.map
-      }
+        }) // map method
+      ); //Main return
+    }// Main If
+    
+    else
+    { 
+      return (
+        stud.map((nam) => {
+          if (nam.firstName.toLowerCase().includes(data) || nam.lastName.toLowerCase().includes(data))
+          {
+            let fullName = nam.firstName + ' ' + nam.lastName;
+            let total = 0;
+            let arrNumbers = nam.grades;
+            for (let i = 0; i < arrNumbers.length; i++){
+              total += Math.floor(arrNumbers[i]);
+            }
+            let average = total/arrNumbers.length;
+            
+            return (
+                <>
+                  <img src={nam.pic} className="leftImag"/>
+                  <div className="box">
+                    <p style={{fontWeight: "bold"}} className="name">{fullName}</p>
+                    <p>Email: {nam.email}</p>
+                    <p>Company: {nam.company}</p>
+                    <p>Skill: {nam.skill}</p>
+                    <p>Average: {average}</p>
+                  </div>
+                </>
+            )
+          }// If condition
+          
+          else
+            return;
+        }) // map method
+      ); //Main return
+    } // Main else condition
+  }
+  
+  return (
+    <main>
+    <div>
+      <input
+          type="text"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          placeholder = {"Search by name"} 
+      />
+      <div>{filter(studentData, data)}</div>
     </div>
     </main>
   )
